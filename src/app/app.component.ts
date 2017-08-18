d962b8bdc60aa0b69239bd71898f9d17e7d456c0
devraj91 <devraj.roy91@gmail.com>
2017-08-18 13:19:54 +0530

    search try with api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'MarkLogic Search';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  http: Http;
+  result = [];
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  performSearch(searchTerm) {
+    console.log('start searching', searchTerm.value);
+    const api_link = this.link + searchTerm.value;
+
+    this.http.request(api_link).subscribe((res: Response) => {
+      this.result = res.json().data;
+      console.log(this.result);
+    });
+  }
 }
